b04e06db958ee979c95e0c6ee3567bf3d0fd1f9d
sim511777 <sim51177@gmail.com>
2023-03-14 20:27:54 +0900

    코드 다이어트

--- ClrTest_TestCs/Program.cs
+++ ClrTest_TestCs/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClrTest;
 
 namespace ClrTest_TestCs {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine($"ClrTest.Managed.Add(3, 4) : {ClrTest.Managed.Add(3, 4)}");
+            Console.WriteLine($"Managed.Add(3, 4) : {Managed.Add(3, 4)}");
             Console.Read();
         }
     }
