77fe18e5a649d818d2303b79aefa3a3ad58f439e
sim511777 <sim51177@gmail.com>
2023-03-14 19:20:46 +0900

    초기 커밋.

---
+++ ClrTest_TestCs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrTest_TestCs {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine($"ClrTest.Managed.Add(3, 4) : {ClrTest.Managed.Add(3, 4)}");
+        }
+    }
+}
